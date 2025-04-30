f2c36b4f007e3b0b810eaf1a72f47923263e4047
Tam <Tam@users.noreply.github.com>
2025-04-30 14:51:10 +0100

    Minimal reproduction

--- graphile.config.mjs
+++ graphile.config.mjs
@@ -1,39 +1,14 @@
 // @ts-check
 import { makePgService } from "@dataplan/pg/adaptors/pg";
-import AmberPreset from "postgraphile/presets/amber";
-import { makeV4Preset } from "postgraphile/presets/v4";
-import { makePgSmartTagsFromFilePlugin } from "postgraphile/utils";
-import { PostGraphileConnectionFilterPreset } from "postgraphile-plugin-connection-filter";
-import { PgAggregatesPreset } from "@graphile/pg-aggregates";
-import { PgManyToManyPreset } from "@graphile-contrib/pg-many-to-many";
-// import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
-import PersistedPlugin from "@grafserv/persisted";
-import { PgOmitArchivedPlugin } from "@graphile-contrib/pg-omit-archived";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-// For configuration file details, see: https://postgraphile.org/postgraphile/next/config
-
-const TagsFilePlugin = makePgSmartTagsFromFilePlugin(`${__dirname}/tags.json5`);
+import { PostGraphileAmberPreset } from "postgraphile/presets/amber";
+import { PgSimplifyInflectionPreset } from "@graphile/simplify-inflection";
 
 /** @satisfies {GraphileConfig.Preset} */
 const preset = {
   extends: [
-    AmberPreset.default ?? AmberPreset,
-    makeV4Preset({
-      /* Enter your V4 options here */
-      graphiql: true,
-      graphiqlRoute: "/",
-    }),
-    PostGraphileConnectionFilterPreset,
-    PgManyToManyPreset,
-    PgAggregatesPreset,
-    // PgSimplifyInflectionPreset
+    PostGraphileAmberPreset,
+    PgSimplifyInflectionPreset,
   ],
-  plugins: [PersistedPlugin.default, PgOmitArchivedPlugin, TagsFilePlugin],
   pgServices: [
     makePgService({
       // Database connection string:
@@ -43,9 +18,12 @@ const preset = {
       // List of schemas to expose:
       schemas: process.env.DATABASE_SCHEMAS?.split(",") ?? ["public"],
       // Enable LISTEN/NOTIFY:
-      pubsub: true,
-    }),
+      pubsub: true
+    })
   ],
+  gather: {
+		pgStrictFunctions: true,
+	},
   grafserv: {
     port: 5678,
     websockets: true,
@@ -54,7 +32,7 @@ const preset = {
   },
   grafast: {
     explain: true,
-  },
+  }
 };
 
 export default preset;
--- schema.sql
+++ schema.sql
@@ -1,1 +1,22 @@
--- Create your database schema here
+drop table if exists public.block_item cascade;
+drop table if exists public.block cascade;
+
+create extension if not exists "uuid-ossp";
+
+create table if not exists public.block (
+  id uuid primary key default public.uuid_generate_v4()
+, name varchar
+);
+
+create table if not exists public.block_item (
+  id uuid primary key references public.block (id) on delete cascade
+, amount int
+);
+
+do $$ begin
+  insert into public.block (id, name)
+  values ('5df56024-61c7-4172-a2ac-6cc3583760f6', 'Example');
+
+  insert into public.block_item (id, amount)
+  values ('5df56024-61c7-4172-a2ac-6cc3583760f6', 10);
+exception when duplicate_object then end $$;
